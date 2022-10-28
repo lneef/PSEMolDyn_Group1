bb5b41c7872ae2c80185e7d33db48d7d0bf0358e
Lukas Neef <lukas.neef@tum.de>
2022-10-28 13:02:14 +0200

    iterator pattern for ParticleContainer

---
+++ src/ParticleContainer.h
@@ -0,0 +1,33 @@
+#include<list>
+#include<iterator>
+
+#include "Particle.h"
+
+class ParticleContainer{
+	private:
+		std::list<Particle> particles;
+	
+	public:
+		class iterator{
+			using iterator_category = std::forward_iterator_tag;
+			using value_type = Particle;
+			using difference_type = std::ptrdiff_t;
+			using pointer = Particle*;
+			using reference = Particle&;
+
+			private:
+				std::list<Particle>::iterator current;
+
+			public:
+				explicit iterator(std::list<Particle>::iterator current);
+				iterator& operator++();
+				iterator operator++(int);
+				reference operator*() const;
+				bool operator==(iterator it);
+				bool operator!=(iterator it);
+
+		};
+
+		iterator begin();
+		iterator end();
+};
