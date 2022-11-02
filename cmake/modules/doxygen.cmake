3d97218130a34852a22a9c4ebaedbf23dec8b5db
Lukas Neef <lukas.neef@tum.de>
2022-11-02 09:43:55 +0100

    moved doxygen build option to right file

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -1,5 +1,12 @@
 # make doc_doxygen optional if someone does not have / like doxygen
 
-# TODO: create CMake build option for the target.
+# CMake build option for the target.
+option(DISABLE_DOXYGEN OFF)
 
-# TODO: Add a custom target for building the documentation.
+# custom target for building the documentation.
+if(NOT DISABLE_DOXYGEN)
+    add_custom_target(doc_doxygen
+    COMMAND doxygen Doxyfile
+    VERBATIM
+)
+endif()
