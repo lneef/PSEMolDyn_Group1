3e3b34339f097de07a99bc7dd2d5482bfd6e7120
dominik rammelsberger <dominik.rammelsberger@tum.de>
2022-11-02 10:30:31 +0100

    gitignore

--- src/Gravitation.cpp
+++ src/Gravitation.cpp
@@ -4,11 +4,13 @@
 
 #include "Gravitation.h"
 
+//calculation new force
 void Gravitation::calculateF(ParticleContainer &particles) {
-    
+    //iterate over all pairs of particles
     for (auto &p1: particles) {
         std::array<double, 3> newF{};
         for (auto &p2: particles) {
+            //check if it the same particle
             if (p1 == p2) {
                 continue;
             }
@@ -19,6 +21,7 @@ void Gravitation::calculateF(ParticleContainer &particles) {
             double norm = calculateNorm(xij);
 
             std::array<double, 3> xji;
+            //simple force calculation for all dimensions
             for (int i = 0; i < 3; i++) {
                 xji[i] = p2.getX()[i] - p1.getX()[i];
                 newF[i] += (p1.getM() * p2.getM() * (xji[i])) / pow(norm, 3);
@@ -28,6 +31,7 @@ void Gravitation::calculateF(ParticleContainer &particles) {
     }
 }
 
+//calculate norm of a vector
 double Gravitation::calculateNorm(std::array<double, 3> x) {
     double norm = sqrt(pow(x[0], 2) + pow(x[1], 2) + pow(x[2], 2));
     return norm;
