6e1ba2551f83646dcba768014df9805fd811759f
Lukas Neef <lukas.neef@tum.de>
2022-11-08 17:48:41 +0000

    updated doxygen

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -1,12 +1,12 @@
 # make doc_doxygen optional if someone does not have / like doxygen
 
 # CMake build option for the target.
-option(DISABLE_DOXYGEN OFF)
+option(DISABLE_DOXYGEN "Disable creation of doxygen target" OFF)
 
 # custom target for building the documentation.
 if(NOT DISABLE_DOXYGEN)
     add_custom_target(doc_doxygen
-    COMMAND doxygen Doxyfile
+    COMMAND doxygen ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile
     VERBATIM
 )
 endif()
