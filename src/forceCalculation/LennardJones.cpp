fcca9b5712424e13c9052d55554281a111d7bc12
Lukas Neef <lukas.neef@tum.de>
2022-11-10 23:48:57 +0000

    better integration of ParticleContainer

--- src/forceCalculation/LennardJones.cpp
+++ src/forceCalculation/LennardJones.cpp
@@ -11,21 +11,25 @@ LennardJones::LennardJones() {
 }
 
 void LennardJones::calculateF(ParticleContainer &particles) {
-    particles.apply([](Particle &p){
-       std::array<double,3> zero{};
-       p.setF(zero);
-    });
 
-    particles.apply2([this](Particle &p1, Particle &p2){
-        std::array<double, 3> xij = p1.getX() - p2.getX();
-        double norm = ArrayUtils::L2Norm(xij);
-        double pow_6 = pow((zeroCrossing / norm), 6);
-        double scalar = ((-24 * depthOfPotentialWell) / pow(norm, 2)) * (pow_6 - 2 * pow(pow_6, 2));
-        std::array<double,3> newF = scalar * xij;
-        p1.setF(p1.getF() + newF);
-        p2.setF(-1 * newF + p2.getF());
+    for(auto &p: particles)
+        p.setF({});
 
+    size_t len = particles.size();
 
-    });
+    for(size_t i = 0; i < len; ++i){
+        for(size_t j = i + 1; j < len; ++j){
+            Particle &p1 = particles[i];
+            Particle &p2 = particles[j];
+
+            std::array<double, 3> xij = p1.getX() - p2.getX();
+            double norm = ArrayUtils::L2Norm(xij);
+            double pow_6 = pow((zeroCrossing / norm), 6);
+            double scalar = ((-24 * depthOfPotentialWell) / pow(norm, 2)) * (pow_6 - 2 * pow(pow_6, 2));
+            std::array<double,3> newF = scalar * xij;
+            p1.setF(p1.getF() + newF);
+            p2.setF(-1 * newF + p2.getF());
+        }
+    }
 }
 
