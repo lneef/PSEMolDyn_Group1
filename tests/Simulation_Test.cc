96ac15de892539409cb9e31a3d540ce4d505634e
Lukas Neef <lukas.neef@tum.de>
2022-11-15 21:22:48 +0000

    added additional test to Simulation_Test

--- tests/Simulation_Test.cc
+++ tests/Simulation_Test.cc
@@ -13,6 +13,7 @@ public:
 };
 
 class OutMock : public outputWriter::FileWriter{
+public:
     MOCK_METHOD(void, plotParticles, (ParticleContainer &particles, const std::string &filename, int iteration) , (override));
 
 };
@@ -21,7 +22,9 @@ TEST(SimulationTest, CallNumber){
     ParticleContainer par{};
     testing::Sequence s1, s2;
     std::unique_ptr<Force> force = std::make_unique<ForceMock>();
+    std::unique_ptr<outputWriter::FileWriter> writer = std::make_unique<OutMock>();
     EXPECT_CALL(dynamic_cast<ForceMock&>(*force), calculateF).Times(12);
-    Simulation sim(par, 1, 11, std::make_unique<OutMock>(), force);
+    EXPECT_CALL(dynamic_cast<OutMock&>(*writer), plotParticles).Times(1);
+    Simulation sim(par, 1, 11, writer, force);
     sim.run();
 }
