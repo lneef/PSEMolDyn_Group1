b3a7188b6f45975174e5b7b7540abf85d2f35d34
Lukas Neef <lukas.neef@tum.de>
2022-12-07 09:18:10 +0000

    fixed test

--- tests/ParserTest.cc
+++ tests/ParserTest.cc
@@ -14,11 +14,10 @@ TEST(ParserTestSite, Basic) {
     auto &particles = lc->getCells();
 
     EXPECT_EQ(lc->size(), 9);
-    EXPECT_EQ(particles[0].size(), 4);
-    EXPECT_EQ(particles[1].size(), 2);
-    EXPECT_EQ(particles[3].size(), 2);
-    EXPECT_EQ(particles[4].size(), 1);
-    EXPECT_DOUBLE_EQ((*particles[0].begin()).getM(), 1.0);
+    EXPECT_EQ(particles[6].size(), 4);
+    EXPECT_EQ(particles[7].size(), 2);
+    EXPECT_EQ(particles[11].size(), 2);
+    EXPECT_EQ(particles[12].size(), 1);
 
 
 }
