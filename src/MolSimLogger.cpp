be80803f3ad5c90937ca4414c2b18fc4fd7c919c
Lukas Neef <lukas.neef@tum.de>
2022-12-08 11:13:00 +0000

    logging

--- src/MolSimLogger.cpp
+++ src/MolSimLogger.cpp
@@ -14,16 +14,18 @@ void MolSimLogger::init() {
     auto logger = std::make_shared<spdlog::logger>("MolSimLogger", sink);
     spdlog::register_logger(logger);
     loggerMolSim = spdlog::get("MolSimLogger");
+    MolSimLogger::logInfo("Logger started!");
 }
 
 void MolSimLogger::init_test() {
     //sink to write messages to log file
-    auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs_test.txt", true);
+    auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs_test.txt", false);
     auto logger = std::make_shared<spdlog::logger>("MolSimLogger_Test", sink);
     spdlog::register_logger(logger);
     //set loglevel for test on debug
     spdlog::set_level(spdlog::level::debug);
     loggerMolSim = spdlog::get("MolSimLogger_Test");
+    MolSimLogger::logInfo("Logger started!");
 }
 
 
