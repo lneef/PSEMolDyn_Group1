7cf997ad5b0e247cfa1a3b462db79b600d75b708
Lukas Neef <lukas.neef@tum.de>
2022-12-14 23:25:54 +0000

    gravitation + bug fixes

--- tests/ParserBrownian_Test.cc
+++ tests/ParserBrownian_Test.cc
@@ -7,10 +7,11 @@ TEST(ParserTestBrownian, ParserTest) {
     std::shared_ptr<Simulation> sth = std::make_shared<Simulation>();
     std::shared_ptr<LinkedCellContainer> lc = std::make_shared<LinkedCellContainer>();
     xml.read(sth, lc);
-
+    auto & force = sth->getForce();
     auto &particles = lc->getCells();
 
     EXPECT_EQ(lc->size(), 9);
+    EXPECT_EQ(typeid(*force), typeid(LennardJones));
     EXPECT_EQ(particles[6].size(), 4);
     EXPECT_EQ(particles[7].size(), 2);
     EXPECT_EQ(particles[11].size(), 2);
