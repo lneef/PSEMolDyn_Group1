7cea82b47b405ebd05b5d58c0e9d1d403cf96a39
Lukas Neef <lukas.neef@tum.de>
2022-12-19 00:13:45 +0000

    comments

--- src/Periodic.h
+++ src/Periodic.h
@@ -5,11 +5,34 @@
 
 #include "container/ParticleContainer.h"
 #include "forceCalculation/LennardJones.h"
-
+/**
+ * @brief class to calculate force in case of periodic boundaries
+ * first internal offset is configured, then apply is calles
+ */
 class Periodic {
+
+    /**
+     * @brief offset to be added to the current position of the particle
+     */
     std::array<double, 3> offset{};
 public:
+    /**
+     * @brief perform force calculation for periodic boundary
+     * @param par ParticleContainer containing particles of he opposite side
+     * @param p current particle
+     * @param fun force calculation routine
+     */
     void apply(ParticleContainer& par, Particle& p, std::function<void(Particle &, Particle &)> &fun);
+
+    /**
+     * @brief add vertical offset
+     * @param domain vertical offset
+     */
     void vert(double domain);
+
+    /**
+     * @brief add horizontal offset
+     * @param domain horizontal offset
+     */
     void hor(double domain);
 };
