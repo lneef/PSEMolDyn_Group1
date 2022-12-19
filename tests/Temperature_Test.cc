9b4137a7f4655b23603c44d4419fb2660a9bb9c1
dominik rammelsberger <dominik.rammelsberger@tum.de>
2022-12-19 11:19:52 +0100

    change test

--- tests/Temperature_Test.cc
+++ tests/Temperature_Test.cc
@@ -4,8 +4,16 @@
 TEST(TemperatureTest, CalcTest){
     std::shared_ptr<Thermostat> thermostat_pointer = std::make_shared<Thermostat>(2.0);
     std::shared_ptr<Container> par = std::make_shared<ParticleContainer>();
-    par->addParticle(Particle ({0.,0.,0.},{2.,1., 0.}, 2.));
+    par->addParticle(Particle ({0.,0.,0.},{3.,3., 0.}, 2.));
+    par->addParticle(Particle ({0.5,0.,0.},{3.,3., 0.}, 2.));
+    par->addParticle(Particle ({1.,0.,0.},{3.,3., 0.}, 2.));
+    par->addParticle(Particle ({0.,0.5,0.},{3.,3., 0.}, 2.));
+    par->addParticle(Particle ({0.5,0.5,0.},{3.,3., 0.}, 2.));
+    par->addParticle(Particle ({1.,0.5,0.},{3.,3., 0.}, 2.));
+    par->addParticle(Particle ({0.,1.,0.},{3.,3., 0.}, 2.));
+    par->addParticle(Particle ({0.5,1.,0.},{3.,3., 0.}, 2.));
+    par->addParticle(Particle ({1.,1.,0.},{3.,3., 0.}, 2.));
     thermostat_pointer->calculateV(par);
 
-    EXPECT_DOUBLE_EQ(thermostat_pointer->getTemp(), 5.);
+    EXPECT_DOUBLE_EQ(thermostat_pointer->getTemp(), 18.);
 }
