af1a9b666d505e6cbcc88c0688c806bae500557b
xiyu <xiyuzhang426@gmail.com>
2022-12-20 21:22:46 +0100

    new optimization of the force-calculation

--- src/forceCalculation/LennardJones.cpp
+++ src/forceCalculation/LennardJones.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include "LennardJones.h"
 #include "container/ParticleContainer.h"
@@ -8,36 +6,44 @@ LennardJones::~LennardJones() = default;
 
 //calculation new force with Lennard-Jones potential
 
-void LennardJones::calculateF(std::shared_ptr<Container> &particles) {
+void LennardJones::calculateF(std::shared_ptr<Container>& particles) {
 
-    particles->apply([](Particle &p) {
+    particles->apply([](Particle& p) {
         p.updateF({});
-    });
-    particles->applyF([](Particle &p1, Particle &p2) {
+        });
+    particles->applyF([](Particle& p1, Particle& p2) {
         calculateF(p1, p2);
-    });
+        });
 
 }
 
 LennardJones::LennardJones() = default;
 
-void LennardJones::calculateF(Particle &p1, Particle &p2) {
+void LennardJones::calculateF(Particle& p1, Particle& p2) {
     double sigma, epsilon;
     if (p1.getType() == p2.getType()) {
         sigma = p1.getSigma();
         epsilon = p1.getEpsilon();
-    } else {
+    }
+    else {
         sigma = (p1.getSigma() + p2.getSigma()) / 2.;
         epsilon = std::sqrt(p1.getEpsilon() * p2.getEpsilon());
     }
 
     std::array<double, 3> xij = p1.getX() - p2.getX();
-    double norm = ArrayUtils::L2Norm(xij);
-    double pow_6 = pow((sigma / norm), 6);
-    double scalar = ((-24 * epsilon) / pow(norm, 2)) * (pow_6 - 2 * pow(pow_6, 2));
+
+    double sum = xij[0] * xij[0] + xij[1] * xij[1] + xij[2] * xij[2];
+    double pow_6 = pow((sigma * sigma)/sum, 3);
+    double scalar = ((-24 * epsilon) / sum) * (pow_6 - 2 * pow(pow_6, 2));
     std::array<double, 3> newF = scalar * xij;
-    p1.setF(p1.getF() + newF);
-    p2.setF(-1 * newF + p2.getF());
+    p1.setF(p1.getF() + newF);  p2.setF(p2.getF() - newF);
+
+    // double norm = ArrayUtils::L2Norm(xij);
+    // double pow_6 = pow((sigma / norm), 6);
+    // double scalar = ((-24 * epsilon) / pow(norm, 2)) * (pow_6 - 2 * pow(pow_6, 2));
+    // std::array<double, 3> newF = scalar * xij;
+    // p1.setF(p1.getF() + newF);
+    // p2.setF(-1 * newF + p2.getF());
 }
 
 
