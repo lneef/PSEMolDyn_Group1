e9973f7c1c411142073b38a01f5ae3484f237578
dominik rammelsberger <dominik.rammelsberger@tum.de>
2022-12-21 15:14:20 +0100

    test checkpointing

--- tests/Checkpoint_Test.cc
+++ tests/Checkpoint_Test.cc
@@ -1,4 +1,5 @@
 #include "gtest/gtest.h"
+#include <gmock/gmock-matchers.h>
 #include "inputReader/xmlReader/XmlReader.h"
 
 TEST(ReaderWriterTest, CheckpointTest) {
@@ -7,29 +8,35 @@ TEST(ReaderWriterTest, CheckpointTest) {
     std::shared_ptr<Simulation> sth1 = std::make_shared<Simulation>();
     std::shared_ptr<LinkedCellContainer> lc1 = std::make_shared<LinkedCellContainer>();
     xml.read(sth1, lc1);
+    sth1->setParticle(lc1);
     sth1->checkpoint("checkpoint");
 
-    /*std::string tes2 = "../tests/testinput/test_checkpoint.xml";
+    std::string tes2 = "../tests/testinput/test_checkpoint.xml";
     XMLReader::XmlReader xml2{tes2};
     std::shared_ptr<Simulation> sth2 = std::make_shared<Simulation>();
     std::shared_ptr<LinkedCellContainer> lc2 = std::make_shared<LinkedCellContainer>();
     xml2.read(sth2, lc2);
+    sth2->setParticle(lc2);
 
-    std::vector<ParticleContainer> pc1 = lc1->getCells();
-    std::vector<ParticleContainer> pc2 = lc2->getCells();
-    std::vector<Particle> particles1;
-    std::vector<Particle> particles2;
+    std::vector<double> vel1;
+    std::vector<double> vel2;
+
+    lc1->apply([&vel1](Particle& p) {
+        std::array<double, 3> pos = p.getX();
+        vel1.push_back(pos[0]);
+        vel1.push_back(pos[1]);
+        vel1.push_back(pos[2]);
+    });
 
 
-    for (int i = 0; i < pc1.size(); i++) {
-        particles1 = pc1[i].getParticles();
-        particles2 = pc2[i].getParticles();
-        for (int z = 0; particles1.size(); z++) {
-            std::array<double, 3> vel1 = particles1[z].getV();
-            std::array<double, 3> vel2 = particles2[z].getV();
-            EXPECT_DOUBLE_EQ(vel1[0], vel2[0]);
-            EXPECT_DOUBLE_EQ(vel1[1], vel2[1]);
-            EXPECT_DOUBLE_EQ(vel1[2], vel2[2]);
-        }
-    }*/
+   lc2->apply([&vel2](Particle& p) {
+        std::array<double, 3> pos = p.getX();
+        vel2.push_back(pos[0]);
+        vel2.push_back(pos[1]);
+        vel2.push_back(pos[2]);
+    });
+
+    for(int i = 0; i < vel1.size(); i++){
+        EXPECT_THAT(vel1, testing::Pointwise(testing::DoubleEq(), vel2));
+    }
 }
