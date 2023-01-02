b70b20602f70da60f5652a82c02a5d06102e2f43
xiyu <xiyuzhang426@gmail.com>
2023-01-02 17:25:17 +0100

    membraneForce added

---
+++ src/forceCalculation/MembraneForce.h
@@ -0,0 +1,43 @@
+#include "Force.h"
+#include "container/ParticleContainer.h"
+
+/**
+ * @brief MembraneForce implements the interface provided by Force
+ *
+ * MembraneForce implements the interface provided by Force for calculating the force between membrane-particles
+*/
+class MembraneForce: public Force {
+public:
+
+    /**
+     * @brief calculates the force effective on the particles inside the given container
+     * @param particles Container holding the particles for which the force is calculated
+     */
+    void calculateF(std::shared_ptr<Container>& particles) override;
+
+    /**
+     * @brief calculates the force between two particles based on the harmonic potential
+     * @param p1 first particle of the interaction
+     * @param p2 second particle of the interaction
+     */
+    static void calculateF(Particle& p1, Particle& p2, double r0, double k);
+
+    ~MembraneForce() override;
+
+    /**
+     * @brief constructor of MembraneForce
+     */
+     MembraneForce(double r0_arg, double k_arg);
+
+private:
+
+    /**
+     * @brief average bond length of a molecule pair
+     */
+    double r0;
+
+    /**
+     * @brief stiffness constant
+     */
+    double k;
+};
