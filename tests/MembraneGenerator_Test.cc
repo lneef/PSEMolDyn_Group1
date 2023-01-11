a20919ce1dbec9eeab1704a77c10ea65cc2750ee
dominik rammelsberger <dominik.rammelsberger@tum.de>
2023-01-11 10:41:39 +0100

    fix generator membrane test

--- tests/MembraneGenerator_Test.cc
+++ tests/MembraneGenerator_Test.cc
@@ -11,20 +11,19 @@ TEST(MembraneGeneratorTest, GenerationTest) {
     double m = 1.;
     std::array<double, 3> v = {0., 0., 0.};
 
-    cg.generateMembraneBrownian(par, x, n, v, h, m, 1, 5, 3, 0.8);
+    cg.generateMembraneBrownian(par, x, n, v, h, m, 1, 1, 5, 3, 0.8);
     std::array<double, 3> pos{0, 1.1, 0};
     std::array<double, 3> zero_force{};
     std::array<double, 3> fz_force{0, 0, 0.8};
     std::array<int, 2> index{0, 1};
     EXPECT_THAT((*par)[1].getX(), testing::Pointwise(testing::DoubleEq(), pos));
     EXPECT_THAT((*par)[1].getF(), testing::Pointwise(testing::DoubleEq(), zero_force));
-    //EXPECT_EQ((*par)[1].getIndex()[0], 0);
-    //EXPECT_EQ((*par)[1].getIndex()[1], 1);
+    EXPECT_EQ((*par)[1].getIndex(), index);
 
-    pos = {17.6, 25.3, 0};
+    pos = {18.7, 26.4, 0};
     index = {17, 24};
-    EXPECT_THAT((*par)[423].getX(), testing::Pointwise(testing::DoubleEq(), pos));
-    //EXPECT_THAT((*par)[423].getF(), testing::Pointwise(testing::DoubleEq(), fz_force));
-    //EXPECT_THAT((*par)[407].getIndex(), testing::Pointwise(testing::DoubleEq(), index));
+    EXPECT_THAT((*par)[449].getX(), testing::Pointwise(testing::DoubleEq(), pos));
+    EXPECT_THAT((*par)[449].getF(), testing::Pointwise(testing::DoubleEq(), fz_force));
+    EXPECT_EQ((*par)[449].getIndex(), index);
 
 }
